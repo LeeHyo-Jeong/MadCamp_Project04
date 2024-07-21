9125cda7de2e761b9cfce7ef4a0e2b7610ab1cda
LeeHyo-Jeong <hyojung040102@naver.com>
2024-07-21 17:36:44 +0900

    add eraser and bucket

--- src/components/drawing.js
+++ src/components/drawing.js
@@ -1,12 +1,15 @@
 import React, { useRef, useEffect, useState } from "react";
 import "../drawing.css";
+import eraser from "../images/eraser.png";
+import bucket from "../images/bucket.png";
 
 const Drawing = ({ onSave }) => {
   const canvasRef = useRef(null);
   const ctxRef = useRef(null);
   const [drawing, setDrawing] = useState(false);
-  const [lineWidth, setLineWidht] = useState(5);
+  const [lineWidth, setLineWidth] = useState(5);
   const [color, setColor] = useState("#000000");
+  const [isErasing, setIsErasing] = useState(false);
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -33,7 +36,9 @@ const Drawing = ({ onSave }) => {
   const draw = (e) => {
     if (!drawing) return;
     ctxRef.current.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
-    ctxRef.current.strokeStyle = color;
+
+    if (isErasing) ctxRef.current.strokeStyle = "white";
+    else ctxRef.current.strokeStyle = color;
     ctxRef.current.lineWidth = lineWidth;
 
     ctxRef.current.stroke();
@@ -45,44 +50,76 @@ const Drawing = ({ onSave }) => {
     onSave(dataURL);
   };
 
+  const paintCanvas = () => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    ctx.fillStyle = color;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  };
+
   return (
-    <div className="drawing-canvas">
-      <canvas
-        ref={canvasRef}
-        onMouseDown={startDrawing}
-        onMouseUp={finishDrawing}
-        onMouseMove={draw}
-      />
-      <div className="color-palette">
-        {[
-          "#B20217",
-          "#E7001D",
-          "#E7492D",
-          "#F2884A",
-          "#F6CA4B",
-          "#FCFB53",
-          "#7DFA54",
-          "#10AD2E",
-          "#12F5F9",
-          "#0FA7F2",
-          "#072EE9",
-          "#6014BE",
-          "#C303B9",
-          "#8A8B90",
-          "#FFFFFF",
-          "#000000",
-        ].map((c) => (
-          <div
-            key={c}
-            style={{ backgroundColor: c }}
-            className="color-swatch"
-            onClick={() => setColor(c)}
-          />
-        ))}
+    <div>
+      <div className="drawing-canvas">
+        <canvas
+          ref={canvasRef}
+          onMouseDown={startDrawing}
+          onMouseUp={finishDrawing}
+          onMouseMove={draw}
+        />
+        <div className="color-palette">
+          {[
+            "#B20217",
+            "#E7001D",
+            "#E7492D",
+            "#F2884A",
+            "#F6CA4B",
+            "#FCFB53",
+            "#7DFA54",
+            "#10AD2E",
+            "#12F5F9",
+            "#0FA7F2",
+            "#072EE9",
+            "#6014BE",
+            "#C303B9",
+            "#8A8B90",
+            "#FFFFFF",
+            "#000000",
+          ].map((c) => (
+            <div
+              key={c}
+              style={{ backgroundColor: c }}
+              className="color-swatch"
+              onClick={() => {
+                setIsErasing(false);
+                setColor(c);
+              }}
+            />
+          ))}
+        </div>
       </div>
-      <button className="drawing-save-button" onClick={saveDrawing}>
-        그림 완성
-      </button>
+      <div className="toolbar">
+        <input
+          type="range"
+          min="1"
+          max="30"
+          value={lineWidth}
+          onChange={(e) => setLineWidth(e.target.value)}
+          className="line-width-slider"
+        />
+        <img
+          src={eraser}
+          className="drawing-icon-button"
+          onClick={() => setIsErasing(true)}
+        />
+        <img
+          src={bucket}
+          className="drawing-icon-button"
+          onClick={() => paintCanvas()}
+        />
+        <button className="drawing-save-button" onClick={saveDrawing}>
+          그림 완성
+        </button>
+      </div>
     </div>
   );
 };
